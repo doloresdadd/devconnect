baff44c127b5b6117ca553c4dc0b18cd8053c9f1
Bethanie <doloresdadd@gmail.com>
2018-06-04 23:53:20 +1000

    Test VSC integrated git

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
+router.get('/test', (req, res) => res.json({
+    msg: 'Users Works'
+}));
 
 module.exports = router;
