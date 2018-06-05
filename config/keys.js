6624797d53d7c9cdaa88c2b07cc7967fb63ae2ea
Bethanie <doloresdadd@gmail.com>
2018-06-05 11:10:44 +1000

    JWT token asigned on successful login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://beth:Bethanie1@ds149278.mlab.com:49278/devconnect'
+    mongoURI: 'mongodb://beth:Bethanie1@ds149278.mlab.com:49278/devconnect',
+    secretOrKey: 'secret'
 }
