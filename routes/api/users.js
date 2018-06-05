2c8b46882abe6a2085c0dfbb26d69a81d6c61d77
Bethanie <doloresdadd@gmail.com>
2018-06-05 10:37:38 +1000

    Encrypt password and save new user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 //Load User Model
 const User = require('../../models/User');
@@ -36,6 +37,16 @@ router.post('/register', (req, res) => {
                     email: req.body.email,
                     avatar,
                     password: req.body.password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
                 })
             }
         })
